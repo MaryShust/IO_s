ab070a0221b81e0cbf852661a47a67663494ffc9
Mirajane <151534082+MaryShust@users.noreply.github.com>
2026-02-20 14:39:10 +0300

    Update kernel.h

--- kernel.h
+++ kernel.h
@@ -9,11 +9,3 @@ struct sbiret {
 #define SBI_ERR_FAILED        -1
 #define SBI_ECALL_0_1_PUTCHAR 0x01
 #define SBI_ECALL_0_1_GETCHAR 0x02
-#define SBI_EXT_BASE          0x10
-#define SBI_EXT_VER           0x00
-#define SBI_EXT_PMU           0x504D55
-#define SBI_EXT_CTR_NUM       0x00
-#define SBI_EXT_CTR_DTLS      0x01
-#define SBI_EXT_HSM           0x48534D
-#define SBI_EXT_SRST          0x53525354
-#define SBI_EXT_SHUTDOWN      0x00
